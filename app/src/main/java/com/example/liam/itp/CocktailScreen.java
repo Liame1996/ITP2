19704af44115b0a4ee2213b7c6ff40bd90a8a57d
x14378581 <john-cahill1997@hotmail.com>
2016-03-29 11:42:28 +0100

    cocktail stuff added

--- app/src/main/java/com/example/liam/itp/CocktailScreen.java
+++ app/src/main/java/com/example/liam/itp/CocktailScreen.java
@@ -2,16 +2,11 @@ package com.example.liam.itp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.method.LinkMovementMethod;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 public class CocktailScreen extends AppCompatActivity {
     ImageButton whiskeyBtn;
