1ea7292eea78111cd3594bc82d2c4bc9565417b3
Liame1996 <liam.english1996@gmail.com>
2016-03-29 12:58:17 +0100

    Diceys Button working

--- app/src/main/java/com/example/liam/itp/FragmentOne.java
+++ app/src/main/java/com/example/liam/itp/FragmentOne.java
@@ -1,6 +1,7 @@
 package com.example.liam.itp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -19,11 +20,21 @@ public class FragmentOne extends Fragment {
     }
 
 
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one_layout,container, false);
+
+        View view = inflater.inflate(R.layout.fragment_one_layout, container,false);
+
+            ImageButton goTo = (ImageButton) view.findViewById(R.id.diceys);
+            goTo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    Intent i = new Intent(getActivity(), DisplayActivity.class);
+                    getActivity().startActivity(i);
+                }
+            });
+        return view;
     }
+
+
 }
